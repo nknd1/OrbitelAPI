4c865073bcfce0e362be3fdbd3e64d6a6793df81
nknd1 <prodblood@gmail.com>
2025-01-06 23:51:14 +0500

    quick fix

---
+++ OrbitelApi/Controllers/TariffController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OrbitelApi.DBContext;
+using OrbitelApi.Models.Dtos;
+
+namespace OrbitelApi.Controllers;
+[ApiController]
+[Route("api/Tariff")]
+public class TariffController(OrbitelContext _context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<TariffDto>>> GetTariff()
+    {
+        var result = await _context.Tariffs.ToListAsync();
+        return Ok(result);
+    }
+}
